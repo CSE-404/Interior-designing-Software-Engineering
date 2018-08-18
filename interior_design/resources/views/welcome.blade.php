262bd4719431f9984291691fef8354c31f443dfb
samia <39670763+SamiaAfsana@users.noreply.github.com>
2018-08-18 13:10:22 +0600

    Add files via upload

---
+++ interior_design/resources/views/welcome.blade.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Interior Design</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+
+   <div id="div1">
+        <div id="div2">
+            <div id= "nav_wrapper">
+                <head><h1>Infinity Software Company Limited</h1></head>
+                <ul>
+                    <li><a href="../" class="active">Home</a></li>
+
+                    <li><a href="#">Design</a>
+                        <ul>
+                            <li><a href="#" class="active1">Kitchen</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Office</a></li>
+                            <li><a href="#">Living room</a></li>
+                            <li><a href="#">Bedroom</a></li>
+                        </ul>
+
+                    </li>
+                    <li><a href="#">Project</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">About Us</a>
+                       <ul>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Map</a></li>
+                        <li><a href="#">Direction</a></li>
+                    </li>
+                </ul>
+  <li><a href="{{ url('/login') }}" target="blank">Login</a></li>
+  <li><a href="{{ url('/register') }}" target="blank">Register</a></li>
+            </div>
+        </div>
+            <div class="divider"></div>
+
+    <h2 class="homesub">Welcome to our website</h2>
+    <h1 class="homedes">Interior</h1>
+    <h1 class="homep">Designing</h1>
+    
+    <br>
+    <div id="wrapper">
+
+      <a href="https://www.facebook.com/Infinity-Software-Company-Limited-1994132243972041/?ref=bookmarks" target="blank"><div class="box" id="facebook">&#62220;</div></a>
+      <a href="https://twitter.com/InfinitySoftwa3" target="blank"><div class="box" id="twitter">&#62217;</div></a>
+       <a href="https://www.instagram.com/infinitysoftwarec/" target="blank"><div class="box" id="instagram">&#62253;</div></a>
+
+
+           <div>
+      <footer
+ class="footer-link">
+          <p class="tm-copyright-text">All Right Reserved By &copy; 2018 Interior Designing</p>
+
+        </footer>
+    </div>
+
+
+
+    </div>
+
+
+
+
+    </body>
+</html>
