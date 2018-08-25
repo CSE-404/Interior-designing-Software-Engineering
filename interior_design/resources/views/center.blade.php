6433300b3d208242c18edd8273d59df12a1f2fdc
mrinmoyee dey amin <39598171+mrinmoyeesinjon@users.noreply.github.com>
2018-08-26 02:14:36 +0600

    Add files via upload

---
+++ interior_design/resources/views/center.blade.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Kitchen Design</title>
+
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="{{url('css/bootstrap.min.css') }}">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" href="{{url('css/magnific-popup.css') }}">                                <!-- Magnific pop up style, http://dimsemenov.com/plugins/magnific-popup/ -->
+    <link rel="stylesheet" href="{{url('css/templatemo-style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">                                   <!-- -->
+</head>
+    <body>  
+            <div id="div1">
+        <div id="div2">
+            <div id= "nav_wrapper">
+                <head><h1>Infinity Software Company Limited</h1></head>
+                <ul>
+                    <li><a href="../" class="active">Home</a></li>
+
+                    <li><a href="#">Design</a>
+                        <ul>
+                            <li><a href="{{ url('/index') }}" class="active1">Kitchen</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Office</a></li>
+                            <li><a href="#">Living room</a></li>
+                            <li><a href="#">Bedroom</a></li>
+                        </ul>
+
+                    </li>
+                    <li><a href="#">Project</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">About Us</a>
+                       <ul>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Map</a></li>
+                        <li><a href="#">Direction</a></li>
+                    </li>
+                </ul>
+  <li><a href="{{ url('/login') }}" target="blank">Login</a></li>
+  <li><a href="{{ url('/register') }}" target="blank">Register</a></li>
+            </div>
+        </div>
+            <div class="divider"></div>       
+        <div class="container">
+            <div class="row">
+                <div class="tm-left-right-container">
+                    <!-- Left column: logo and menu -->
+                    <div class="tm-blue-bg tm-left-column">                        
+                        <div class="tm-logo-div text-xs-center">
+                            
+                            <h1 class="tm-site-name">Infinity Software Company Limited</h1>
+                        </div>
+                        <nav class="tm-main-nav">
+                            <ul class="tm-main-nav-ul">
+                                <li class="tm-nav-item">
+                                    <a href="{{ url('/idea') }}" class="tm-nav-item-link">Kitchen Ideas</a>
+                                </li>
+                                <li class="tm-nav-item">
+                                    <a href="{{ url('/general') }}" class="tm-nav-item-link">General Kitchen Ideas</a>
+                                </li>
+                                </li>
+                                <li class="tm-nav-item">
+                                     <a href="{{ url('/back') }}" class="tm-nav-item-link">Kitchen Backsplash Idea</a>
+                                </li>
+                                    <li class="tm-nav-item">
+                                    <a href="{{ url('/cabinet') }}" class="tm-nav-item-link">Kitchen Cabinet Ideas</a>
+                                </li>
+                                </li>
+                                    <li class="tm-nav-item">
+                                    <a href="{{ url('/color') }}" class="tm-nav-item-link">Kitchen Color Ideas</a>
+                                </li>
+                                </li>
+                                    <li class="tm-nav-item">
+                                    <a href="{{ url('/center') }}" class="tm-nav-item-link">Kitchen Centertop Ideas</a>
+                                </li>
+                               
+                                
+
+                         
+                        </nav>                                         
+                    </div> <!-- Left column: logo and menu -->
+                    
+                    
+                    <!-- Right column: content -->
+                    <div class="tm-right-column">
+                        <figure>
+                            <img src="img/cen.jpg" alt="Header image" class="img-fluid">    
+                        </figure>
+
+                        <div class="tm-content-div">
+                            <!-- Welcome section -->
+                            <section id="welcome" class="tm-section">
+                                <header>
+                                    <h2 class="tm-blue-text tm-welcome-title tm-margin-b-45">Kitchen Centertop Ideas</h2>
+                                </header>
+                                <p>It's a huge decision, choosing a material for your kitchen countertop. Huge both in terms of  cost and in their impact on the aesthetics and practical function of your kitchen. Top-end, premium kitchen countertop materials are a significant part of a kitchen remodeling budget. A well-chosen countertop makes your kitchen more useful and easier to maintain, and it can also serve as an important design element of your kitchen.</p>
+                                <p>The range of materials that can be used for kitchen countertops is amazingly diverse, and some materials on the list may be surprising to you. Each material has its advantages and disadvantages, all of which need to be weighed when choosing. The more esoteric materials, such as bamboo, zinc, and stainless steel. may prove to be too expensive and hard to maintain for most people. For most homeowners, the more practical options will include materials such as slab granite, engineered stone (quartz), solid surface material, as well as traditional laminates and ceramic tile. </p>
+                                <p>We’ve broken down all the elements of a renovation and pulled together our top kitchen ideas into the ultimate guide that will help you create your best kitchen yet.</p>
+                            </section>
+                            <!-- About section -->
+                            <section id="about" class="tm-section">
+                                <div class="row">                                                                
+                                    <div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 push-lg-4 push-md-5">
+                                        <header>
+                                            <h2 class="tm-blue-text tm-section-title tm-margin-b-45">About our project</h2>
+                                        </header>
+                                        <p>We build a .......</p>
+                                        <p>by done this...........
+                                        .....................................</p>
+                                        <a href="#" class="tm-button tm-button-wide">Read More</a>  
+                                    </div>
+
+                                    <div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 pull-lg-8 pull-md-7 tm-about-img-container">
+                                        
+                                    </div>  
+                                </div>                            
+                            </section>  
+
+                            <!-- Gallery One section -->     
+                            <section id="Kitchen Centertop Ideas" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Kitchen Centertop Ideas</h2></header>
+                                <div class="tm-gallery-container tm-gallery-1">
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/cen1.JPEG "><img src="img/cen1.JPEG " alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/cen2.jpg"><img src="img/cen2.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/cen3.JPEG "><img src="img/cen3.JPEG " alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/cen4.jpg"><img src="img/cen4.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/cen5.jpg"><img src="img/cen5.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>                                    
+                                </div>
+                            </section>
+
+                            <!-- Second Gallery section -->     
+                            <section id="secondgallery" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Second Gallery</h2></header>
+                                <div class="tm-gallery-container tm-gallery-2">
+                                    <div class="tm-img-container tm-img-container-2">
+                                        <a href="img/cen6.JPEG "><img src="img/cen6.JPEG " alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-2">
+                                        <a href="img/cen7.jpg"><img src="img/cen7.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-2">
+                                        <a href="img/cen8.jpg"><img src="img/cen8.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>                                    
+                                </div>
+                            </section>
+
+                            <!-- Third Gallery section -->     
+                            <section id="thirdgallery" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Third Gallery</h2></header>
+                                <div class="tm-gallery-container tm-gallery-3">
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/cen9.JPEG "><img src="img/cen9.JPEG " alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/cen10.jpg"><img src="img/cen10.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/cen11.jpg"><img src="img/cen11.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/cen12.jpg"><img src="img/cen12.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+                                    </div>                                    
+                                </div>
+                            </section>
+
+                            <!-- Contact Us section -->
+                            <section id="contact" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Contact Us</h2></header>
+
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <form action="#contact" method="post" class="contact-form">
+                                            <div class="form-group">
+                                                <input type="text" id="contact_name" name="contact_name" class="form-control" placeholder="Name"  required/>
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="email" id="contact_email" name="contact_email" class="form-control" placeholder="Email"  required/>
+                                            </div>
+                                            <div class="form-group">
+                                                <textarea id="contact_message" name="contact_message" class="form-control" rows="9" placeholder="Message" required></textarea>
+                                            </div>                                            
+                                            <button type="submit" class="float-right tm-button">Send</button>
+                                        </form>    
+                                    </div>
+                                    
+                                    <div class="col-lg-6 tm-contact-right">
+                                        <p>
+                                        Infinity Software Company Limited a company that.... 
+                                        </p>
+                                        <address>
+                                            Infinity Software Company Limited<br>
+                                            Road No- 7/A, House-185/Kha, Dhanmondi, Dhaka-1209. Cell: +8801760150897<br>
+                                            
+                                        </address>
+                                    </div>
+                                </div>
+                                
+                            </section>
+                            <footer>
+                                <p class="tm-copyright-p">Copyright &copy; <span class="tm-current-year">2018</span> Infinity Software Company Limited
+                                
+                               
+                            </footer>
+                        </div>  
+                        
+                    </div> <!-- Right column: content -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+                
+        <!-- load JS files -->
+        <script src="js/jquery-1.11.3.min.js"></script>             <!-- jQuery (https://jquery.com/download/) -->
+        <script src="js/jquery.magnific-popup.min.js"></script>     <!-- Magnific pop-up (http://dimsemenov.com/plugins/magnific-popup/) -->
+        <script src="js/jquery.singlePageNav.min.js"></script>      <!-- Single Page Nav (https://github.com/ChrisWojcik/single-page-nav) -->
+        <script>     
+       
+            $(document).ready(function(){
+
+                // Single page nav
+                $('.tm-main-nav').singlePageNav({
+                    'currentClass' : "active",
+                    offset : 20
+                });
+
+                // Magnific pop up
+                $('.tm-gallery-1').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-gallery-2').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-gallery-3').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-current-year').text(new Date().getFullYear());                
+            });
+        </script> 
+        <div id="wrapper">
+
+      <a href="https://www.facebook.com/Infinity-Software-Company-Limited-1994132243972041/?ref=bookmarks" target="blank"><div class="box" id="facebook">&#62220;</div></a>
+      <a href="https://twitter.com/InfinitySoftwa3" target="blank"><div class="box" id="twitter">&#62217;</div></a>
+       <a href="https://www.instagram.com/infinitysoftwarec/" target="blank"><div class="box" id="instagram">&#62253;</div></a>             
+</body>
+</html>
