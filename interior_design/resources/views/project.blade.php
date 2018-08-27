e6438ad558832dd5dde283c0ac6e3a42f3133f08
NadiaRahman <40435622+NadiaRahman@users.noreply.github.com>
2018-08-27 23:20:04 +0600

    Add files via upload

---
+++ interior_design/resources/views/project.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> project </title>
+	<link rel="stylesheet" type="text/css" href="{{ url('css/project.css') }}"> 
+	<link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">
+</head>
+<body>
+	<div id="div1">
+        <div id="div2">
+            <div id= "nav_wrapper">
+                <head><h1>Infinity Software Company Limited</h1></head>
+                <ul>
+                    <li><a href="../" class="active">Home</a></li>
+
+                    <li><a href="{{ url('/index') }}">Design</a>
+                        <ul>
+                            <li><a href="{{ url('/index') }}" class="active1">Kitchen</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Office</a></li>
+                            <li><a href="#">Living room</a></li>
+                            <li><a href="#">Bedroom</a></li>
+                        </ul>
+
+                    </li>
+                    <li><a href="{{ url('/project') }}">Project</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">About Us</a>
+                       <ul>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Map</a></li>
+                        <li><a href="#">Direction</a></li>
+                    </li>
+                </ul>
+  <li><a href="{{ url('/login') }}" target="blank">Login</a></li>
+  <li><a href="{{ url('/register') }}" target="blank">Register</a></li>
+            </div>
+        </div>
+            <div class="divider"></div>
+	<h1 class="homedes">Our Interior Complete Project </h1>
+
+	<br>
+	<div style="text-align: center;">
+	<img src="img/pro1.jpg" height="300" width="500" />
+	<img src="img/pro2.jpg" height="300" width="500"/>
+	<img src="img/pro3.jpg" height="300" width="500"/>
+	<img src="img/pro4.jpg" height="300" width="500"/>
+	<img src="img/pro5.jpg" height="300" width="500"/>
+	<img src="img/pro 6.jpg" height="300" width="500"/>
+	</div>
+
+
+<br>
+<br>
+<h2 class="homep"> Project On Going</h2>	
+
+ <br>
+ <br>
+ <div style="text-align: center;">
+<img src="img/pro7.jpg" height="300" width="500"/>
+
+<img src="img/pro8.jpg" height="300" width="500"/>
+
+<img src="img/pro9.jpg" height="300" width="500"/>
+
+<img src="img/pro10.jpg" height="300" width="500"/>
+
+<img src="img/pro11.jpg" height="300" width="500"/>
+
+<img src="img/pro12.jpg" height="300" width="500"/>
+</div>
+<footer>
+                                <p class="tm-copyright-p">Copyright &copy; <span class="tm-current-year">2018</span> Infinity Software Company Limited
+ <a href="https://www.facebook.com/Infinity-Software-Company-Limited-1994132243972041/?ref=bookmarks" target="blank"><div class="box" id="facebook">&#62220;</div></a>
+      <a href="https://twitter.com/InfinitySoftwa3" target="blank"><div class="box" id="twitter">&#62217;</div></a>
+       <a href="https://www.instagram.com/infinitysoftwarec/" target="blank"><div class="box" id="instagram">&#62253;</div></a>  
+</p></footer>
+</body>
+</html>
