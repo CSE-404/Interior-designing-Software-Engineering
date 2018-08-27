f364bb50541830b2e9c87da867c7e0069c347b3b
Md. Asifuzzaman Jishan <32738421+jishan900@users.noreply.github.com>
2018-08-28 02:35:55 +0600

    Add files via upload

---
+++ interior_design/resources/views/contact.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Kitchen Design</title>
+<!--
+Neaty HTML Template
+http://www.templatemo.com/tm-501-neaty
+-->
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="{{url('css/bootstrap.min.css') }}">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" href="{{url('css/magnific-popup.css') }}">                                <!-- Magnific pop up style, http://dimsemenov.com/plugins/magnific-popup/ -->
+    <link rel="stylesheet" href="{{url('css/templatemo-style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}"></head>
+<body>
+<div id="div1">
+        <div id="div2">
+            <div id= "nav_wrapper">
+                <head><h1>Infinity Software Company Limited</h1></head>
+                <ul>
+                    <li><a href="../" class="active">Home</a></li>
+
+                    <li><a href="{{ url('/index') }}">Design</a>
+                        <ul>
+                            <li><a href="{{ url('/index') }}" class="active1">Kitchen</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Office</a></li>
+                            <li><a href="#">Living room</a></li>
+                            <li><a href="#">Bedroom</a></li>
+                        </ul>
+
+                    </li>
+                    <li><a href="{{ url('/project') }}">Project</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="{{ url('/contact') }}">About Us</a>
+                       <ul>
+                        <li><a href="{{ url('/contact') }}">Contact</a></li>
+                        <li><a href="{{ url('/map') }}">Map</a></li>
+                        <li><a href="#">Direction</a></li>
+                    </li>
+                </ul>
+  <li><a href="{{ url('/login') }}" target="blank">Login</a></li>
+  <li><a href="{{ url('/register') }}" target="blank">Register</a></li>
+            </div>
+        </div>
+            <div class="divider"></div>
+    <h1 class="homedes">Interior Designing</h1>
+
+<h2 style="text-align:left;color: white"><b>Contact Address-</h2></b>
+<p style="text-align:left;color: white">Infinity Software Company Limited</p>
+<p style="text-align:left;color: white">Road No- 7/A, House No-185/Kha,@ Satmasjid Road, Dhanmondi</p>
+<p style="text-align:left;color: white">Dhaka-1209, Bangladesh</p>
+<p style="text-align:left;color: white">Cell: 01977-239551, 01703-332454, 01683-561942, 01920-399341.</p>
+<p style="text-align:left;color: white">For Emergency Contact: 01684-800657, 01957-550258, 01760-150897, 01725-285058.</p>
+<div id="wrapper">
+      <a href="https://www.facebook.com/Infinity-Software-Company-Limited-1994132243972041/?ref=bookmarks" target="blank"><div class="box" id="facebook">&#62220;</div></a>
+      <a href="https://twitter.com/InfinitySoftwa3" target="blank"><div class="box" id="twitter">&#62217;</div></a>
+       <a href="https://www.instagram.com/infinitysoftwarec/" target="blank"><div class="box" id="instagram">&#62253;</div></a> 
+       <div>
+    </div>
+</body>
+<html>
