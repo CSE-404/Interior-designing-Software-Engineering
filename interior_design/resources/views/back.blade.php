201c28784f82b76835071aedeb71f30cae6cdc70
Md. Asifuzzaman Jishan <32738421+jishan900@users.noreply.github.com>
2018-08-28 02:47:43 +0600

    Update back.blade.php

--- interior_design/resources/views/back.blade.php
+++ interior_design/resources/views/back.blade.php
@@ -38,7 +38,7 @@
                        <ul>
                         <li><a href="{{ url('/contact') }}">Contact</a></li>
                         <li><a href="{{ url('/map') }}">Map</a></li>
-                        <li><a href="{{ url('/contact') }}">Direction</a></li>
+                        <li><a href="#">Direction</a></li>
                     </li>
                 </ul>
   <li><a href="{{ url('/login') }}" target="blank">Login</a></li>
