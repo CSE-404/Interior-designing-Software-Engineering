6e8851e5ed8d62389389af1c1875c9b14ca20df4
Md. Asifuzzaman Jishan <32738421+jishan900@users.noreply.github.com>
2018-08-28 15:09:30 +0600

    Add files via upload

---
+++ interior_design/resources/views/direction.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Kitchen Design</title>
+<!--
+Neaty HTML Template
+http://www.templatemo.com/tm-501-neaty
+-->
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="{{url('css/bootstrap.min.css') }}">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" href="{{url('css/magnific-popup.css') }}">                                <!-- Magnific pop up style, http://dimsemenov.com/plugins/magnific-popup/ -->
+    <link rel="stylesheet" href="{{url('css/templatemo-style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}"></head>
+<body>
+<div id="div1">
+        <div id="div2">
+            <div id= "nav_wrapper">
+                <head><h1>Infinity Software Company Limited</h1></head>
+                <ul>
+                    <li><a href="../" class="active">Home</a></li>
+
+                    <li><a href="{{ url('/index') }}">Design</a>
+                        <ul>
+                            <li><a href="{{ url('/index') }}" class="active1">Kitchen</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Office</a></li>
+                            <li><a href="#">Living room</a></li>
+                            <li><a href="#">Bedroom</a></li>
+                        </ul>
+
+                    </li>
+                    <li><a href="{{ url('/project') }}">Project</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="{{ url('/contact') }}">About Us</a>
+                       <ul>
+                        <li><a href="{{ url('/contact') }}">Contact</a></li>
+                        <li><a href="{{ url('/map') }}">Map</a></li>
+                        <li><a href="{{ url('/direction') }}">Direction</a></li>
+                    </li>
+                </ul>
+  <li><a href="{{ url('/login') }}" target="blank">Login</a></li>
+  <li><a href="{{ url('/register') }}" target="blank">Register</a></li>
+            </div>
+        </div>
+            <div class="divider"></div>
+    <h1 class="homedes">Interior Designing</h1>
+<iframe align="left" src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d29218.691161856317!2d90.37518665254574!3d23.73538066417488!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m5!1s0x3755b8f85fd0fa61%3A0xeb7c4af37f20b840!2sGulistan+Shopping+Complex%2C+Bangabadhu+Avenue%2C+Dhaka!3m2!1d23.7245921!2d90.411718!4m3!3m2!1d23.7433581!2d90.373503!5e0!3m2!1sen!2sbd!4v1535398140965" width="800" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<div id="wrapper">
+      <a href="https://www.facebook.com/Infinity-Software-Company-Limited-1994132243972041/?ref=bookmarks" target="blank"><div class="box" id="facebook">&#62220;</div></a>
+      <a href="https://twitter.com/InfinitySoftwa3" target="blank"><div class="box" id="twitter">&#62217;</div></a>
+       <a href="https://www.instagram.com/infinitysoftwarec/" target="blank"><div class="box" id="instagram">&#62253;</div></a> 
+       <div>
+    </div>
+</body>
+<html>
