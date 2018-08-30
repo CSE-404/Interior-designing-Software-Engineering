576999d132ac2201ce69d8a0b15c46300e009a22
mrinmoyee dey amin <39598171+mrinmoyeesinjon@users.noreply.github.com>
2018-08-31 00:06:08 +0600

    Add files via upload

---
+++ interior_design/resources/views/index2.blade.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Washroom Design</title>
+
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="{{url('css/bootstrap.min.css') }}">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" href="{{url('css/magnific-popup.css') }}">                                <!-- Magnific pop up style, http://dimsemenov.com/plugins/magnific-popup/ -->
+    <link rel="stylesheet" href="{{url('css/templatemo-style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">                                   <!-- Templatemo style -->
+
+</head>
+    <body>  
+    <div id="div1">
+        <div id="div2">
+            <div id= "nav_wrapper">
+                <head><h1>Infinity Software Company Limited</h1></head>
+                <ul>
+                    <li><a href="../" class="active">Home</a></li>
+
+                    <li><a href="{{ url('/index') }}">Design</a>
+                        <ul>
+                            <li><a href="{{ url('/index') }}" class="active1">Kitchen</a></li>
+                            <li><a href="{{ url('/index2') }}">Home</a></li>
+                            <li><a href="#">Office</a></li>
+                            <li><a href="#">Living room</a></li>
+                            <li><a href="#">Bedroom</a></li>
+                        </ul>
+
+                    </li>
+                    <li><a href="{{ url('/project') }}">Project</a></li>
+                    <li><a href="{{ url('/news') }}">News</a></li>
+                    <li><a href="{{ url('/contact') }}">About Us</a>
+                       <ul>
+                        <li><a href="{{ url('/contact') }}">Contact</a></li>
+                        <li><a href="{{ url('/map') }}">Map</a></li>
+                        <li><a href="{{ url('/direction') }}">Direction</a></li>
+                    </li>
+                </ul>
+  <li><a href="{{ url('/login') }}" target="blank">Login</a></li>
+  <li><a href="{{ url('/register') }}" target="blank">Register</a></li>
+            </div>
+        </div>
+            <div class="divider"></div>       
+        <div class="container">
+            <div class="row">
+                <div class="tm-left-right-container">
+                    <!-- Left column: logo and menu -->
+                    <div class="tm-blue-bg tm-left-column">                        
+                        <div class="tm-logo-div text-xs-center">
+                            
+                            <h1 class="tm-site-name">Infinity Software Company Limited</h1>
+                        </div>
+                        <nav class="tm-main-nav">
+                            <ul class="tm-main-nav-ul">
+                                <li class="tm-nav-item">
+                                    <a href="{{ url('/wash') }}" class="tm-nav-item-link">Washroom Design</a>
+                                </li>
+                                <li class="tm-nav-item">
+                                    <a href="{{ url('/dining') }}" class="tm-nav-item-link">Dining Design</a>
+                                </li>
+                                </li>
+                                <li class="tm-nav-item">
+                                    <a href="{{ url('/balcony') }}" class="tm-nav-item-link">Balcony Design</a>
+                                </li>
+
+
+                                
+                    </div> <!-- Left column: logo and menu -->
+                    
+                    
+                    <!-- Right column: content -->
+                    <div class="tm-right-column">
+                        <figure>
+                            <img src="img/waco.jpg" alt="Header image" class="img-fluid">    
+                        </figure>
+
+                        <div class="tm-content-div">
+                            <!-- Welcome section -->
+                            <section id="welcome" class="tm-section">
+                                <header>
+                                    <h2 class="tm-blue-text tm-welcome-title tm-margin-b-45">Welcome to Wash Design</h2>
+                                </header>
+                                <p>Small bathrooms may seem like a difficult design task to take on; however, these spaces may introduce a clever design challenge to add to your plate. Creating a functional and storage-friendly bathroom may be just what your home needs.
+
+                                Every design element in a small bathroom should have a purpose and be functional in some way or another to create a space-saving sanctuary. Before you dive in, if you are looking for space-saving furniture ideas, be sure to check out our top products for small bathrooms.</p>
+                            </section>
+                                                   
+                            </section>  
+
+                            <!-- Gallery One section -->     
+                            <section id="galleryone" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Gallery One</h2></header>
+                                <div class="tm-gallery-container tm-gallery-1">
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/wa1.jpg"><img src="img/wa1.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/wa2.jpg"><img src="img/wa2.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/wa3.jpg"><img src="img/wa3.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/wa4.jpg"><img src="img/wa4.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/wa5.jpg"><img src="img/wa5.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>                                    
+                                </div>
+                            </section>
+
+                            <!-- Second Gallery section -->     
+                            <section id="secondgallery" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Second Gallery</h2></header>
+                                <div class="tm-gallery-container tm-gallery-2">
+                                    <div class="tm-img-container tm-img-container-2">
+                                        <a href="img/wa6.jpg"><img src="img/wa6.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-2">
+                                        <a href="img/wa7.jpg"><img src="img/wa7.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-2">
+                                        <a href="img/wa8.jpg"><img src="img/wa8.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>                                    
+                                </div>
+                            </section>
+
+                            <!-- Third Gallery section -->     
+                            <section id="thirdgallery" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Third Gallery</h2></header>
+                                <div class="tm-gallery-container tm-gallery-3">
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/wa9.jpg"><img src="img/wa9.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/wa10.jpg"><img src="img/wa10.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/wa11.jpg"><img src="img/wa11.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/wa12.jpg"><img src="img/wa12.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+                                    </div>                                    
+                                </div>
+                            </section>
+
+                            <!-- Contact Us section -->
+                            <section id="contact" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Contact Us</h2></header>
+
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <form action="#contact" method="post" class="contact-form">
+                                            <div class="form-group">
+                                                <input type="text" id="contact_name" name="contact_name" class="form-control" placeholder="Name"  required/>
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="email" id="contact_email" name="contact_email" class="form-control" placeholder="Email"  required/>
+                                            </div>
+                                            <div class="form-group">
+                                                <textarea id="contact_message" name="contact_message" class="form-control" rows="9" placeholder="Message" required></textarea>
+                                            </div>                                            
+                                            <button type="submit" class="float-right tm-button">Send</button>
+                                        </form>    
+                                    </div>
+                                    
+                                    <div class="col-lg-6 tm-contact-right">
+                                        <p>
+                                        Infinity Software Company Limited a company that.... 
+                                        </p>
+                                        <address>
+                                            Infinity Software Company Limited<br>
+                                            Road No- 7/A, House-185/Kha, Dhanmondi, Dhaka-1209. Cell: +8801760150897<br>
+                                            
+                                        </address>
+                                    </div>
+                                </div>
+                                
+                            </section>
+                            <footer>
+                                <p class="tm-copyright-p">Copyright &copy; <span class="tm-current-year">2018</span> Infinity Software Company Limited
+                                
+                               
+                            </footer>
+                        </div>  
+                        
+                    </div> <!-- Right column: content -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+                
+        <!-- load JS files -->
+        <script src="js/jquery-1.11.3.min.js"></script>             <!-- jQuery (https://jquery.com/download/) -->
+        <script src="js/jquery.magnific-popup.min.js"></script>     <!-- Magnific pop-up (http://dimsemenov.com/plugins/magnific-popup/) -->
+        <script src="js/jquery.singlePageNav.min.js"></script>      <!-- Single Page Nav (https://github.com/ChrisWojcik/single-page-nav) -->
+        <script>     
+       
+            $(document).ready(function(){
+
+                // Single page nav
+                $('.tm-main-nav').singlePageNav({
+                    'currentClass' : "active",
+                    offset : 20
+                });
+
+                // Magnific pop up
+                $('.tm-gallery-1').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-gallery-2').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-gallery-3').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-current-year').text(new Date().getFullYear());                
+            });
+        </script> 
+        <div id="wrapper">
+
+      <a href="https://www.facebook.com/Infinity-Software-Company-Limited-1994132243972041/?ref=bookmarks" target="blank"><div class="box" id="facebook">&#62220;</div></a>
+      <a href="https://twitter.com/InfinitySoftwa3" target="blank"><div class="box" id="twitter">&#62217;</div></a>
+       <a href="https://www.instagram.com/infinitysoftwarec/" target="blank"><div class="box" id="instagram">&#62253;</div></a>                
+</body>
+</html>
