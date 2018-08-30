fa89523bbd0adf593c79eb7bfe74e41cadaa877d
Md. Asifuzzaman Jishan <32738421+jishan900@users.noreply.github.com>
2018-08-30 20:27:48 +0600

    Update web.php

--- interior_design/routes/web.php
+++ interior_design/routes/web.php
@@ -29,3 +29,4 @@ Route::get('/project','ProjectPageController@showProjectpage');
 Route::get('/map','MapPageController@showMappage');
 Route::get('/contact','ContactPageController@showContactpage');
 Route::get('/direction','DirectionPageController@showDirectionpage');
+Route::get('/news','NewsPageController@showNewspage');
