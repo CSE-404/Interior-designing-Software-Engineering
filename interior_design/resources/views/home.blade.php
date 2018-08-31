814083b84e4080050f489dd03ca261a8075a5a3d
Md. Asifuzzaman Jishan <32738421+jishan900@users.noreply.github.com>
2018-09-01 01:06:09 +0600

    Update home.blade.php

--- interior_design/resources/views/home.blade.php
+++ interior_design/resources/views/home.blade.php
@@ -1,24 +1,101 @@
 @extends('layouts.app')
 
 @section('content')
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
 <title>Interior Design</title>
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+ <link rel="stylesheet" href="css/style1.css">
 
-                <div class="panel-body">
+ </head>
+ <div id="header">
+        <div class="logo">
+            <a href="../">Infinity Software Company Limited</a>
+        </div>
+    </div>
+    <a class="mobile">MENU</a>
+
+
+    <div id="container">
+
+        <div class="sidebar">
+            <ul id="nav">
+                <li>
+                    <span>
+          <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
+        </span>
+                    <a class="selected" href="#">My Profile</a>
+                </li>
+                <li>
+                    <span>
+          <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
+        </span><a href="#">Our Interior Designer Team</a>
+                </li>
+                <li>
+                    <span>
+          <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
+        </span><a href="#">Hire a designer</a>
+                </li>
+                <li>
+                    <span>
+          <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
+        </span><a href="#">Our Suppliers Team</a>
+                </li>
+                <li>
+                    <span>
+          <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
+        </span><a href="#">Product Order</a>
+                </li>
+                <li>
+                    <span>
+          <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
+        </span><a href="#">Contact us</a>
+                </li>
+            </ul>
+
+        </div>
+
+        <div class="content">
+            <h1>Dashboard</h1>
+            <p>Interior design is the art and science of enhancing the interior of a building to achieve a healthier and more aesthetically pleasing environment for the people using the space. An interior designer is someone who plans, researches, coordinates, and manages such projects. Interior design is a multifaceted profession that includes conceptual development, space planning, site inspections, programming, research, communicating with the stakeholders of a project, construction management, and execution of the design.</p>
+            <div id="box">
+                <div class="box-top">News</div>
+                <div class="box-panel">We are happily to inform you recently three designers join in our firm.</div>
+            </div>
+
+            <div id="box">
+                <div class="box-top">News</div>
+                <div class="box-panel">We recently added office interior design in our service.</div>
+            </div>
+
+
+            <div id="box">
+                <div class="box-top">News</div>
+                <div class="box-panel">We provide raw materials supply system from our dealer in a cheap rate. Now you can place order in our website using login system.</div>
+            </div>
+             <div id="box">
+                <div class="box-top">News</div>
+                <div class="box-panel">We also provide designers for your service. Now you can choose designer from our website using login system.</div>
+            </div>
+        </div>
+
+
+    </div>
+    <!-- #container -->
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script src="js/index.js"></script>
+                    <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+@endsection 
+</body>
+</html>
