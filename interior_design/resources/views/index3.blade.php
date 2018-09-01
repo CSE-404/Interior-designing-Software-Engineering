ba3f59e050b444015e1394a3e8ac958fffc76c42
tareqrahman111 <40437000+tareqrahman111@users.noreply.github.com>
2018-09-02 03:37:13 +0600

    Add files via upload

---
+++ interior_design/resources/views/index3.blade.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Kitchen Design</title>
+<!--
+Neaty HTML Template
+http://www.templatemo.com/tm-501-neaty
+-->
+    <!-- load stylesheets -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+    <link rel="stylesheet" href="{{url('css/bootstrap.min.css') }}">                                      <!-- Bootstrap style -->
+    <link rel="stylesheet" href="{{url('css/magnific-popup.css') }}">                                <!-- Magnific pop up style, http://dimsemenov.com/plugins/magnific-popup/ -->
+    <link rel="stylesheet" href="{{url('css/templatemo-style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ url('css/style.css') }}">                               <!-- Templatemo style -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+          <![endif]-->
+</head>
+    <body> 
+     <div id="div1">
+        <div id="div2">
+            <div id= "nav_wrapper">
+                <head><h1>Infinity Software Company Limited</h1></head>
+                <ul>
+                    <li><a href="../" class="active">Home</a></li>
+
+                    <li><a href="{{ url('/index') }}">Design</a>
+                        <ul>
+                            <li><a href="{{ url('/index') }}" class="active1">Kitchen</a></li>
+                            <li><a href="{{ url('/index2') }}">Home</a></li>
+                            <li><a href="#">Office</a></li>
+                            <li><a href="{{ url('/index3') }}">Living room</a></li>
+                            <li><a href="#">Bedroom</a></li>
+                        </ul>
+
+                    </li>
+                    <li><a href="{{ url('/project') }}">Project</a></li>
+                    <li><a href="{{ url('/news') }}">News</a></li>
+                    <li><a href="{{ url('/contact') }}">About Us</a>
+                       <ul>
+                        <li><a href="{{ url('/contact') }}">Contact</a></li>
+                        <li><a href="{{ url('/map') }}">Map</a></li>
+                        <li><a href="{{ url('/direction') }}">Direction</a></li>
+                    </li>
+                </ul>
+  <li><a href="{{ url('/login') }}" target="blank">Login</a></li>
+  <li><a href="{{ url('/register') }}" target="blank">Register</a></li>
+            </div>
+        </div>
+            <div class="divider"></div>        
+        <div class="container">
+            <div class="row">
+                <div class="tm-left-right-container">
+                    <!-- Left column: logo and menu -->
+                    <div class="tm-blue-bg tm-left-column">                        
+                        <div class="tm-logo-div text-xs-center">
+                            
+                            <h1 class="tm-site-name">Infinity Software Company Limited</h1>
+                        </div>
+
+                                    
+
+                         
+                            </ul>
+                        </nav>                                         
+                    </div> <!-- Left column: logo and menu -->
+                    
+                    
+                    <!-- Right column: content -->
+                    <div class="tm-right-column">
+                        <figure>
+                            <img src="img/cover13.jpg" alt="Header image" class="img-fluid">    
+                        </figure>
+
+                        <div class="tm-content-div">
+                            <!-- Welcome section -->
+                            <section id="welcome" class="tm-section">
+                                <header>
+                                    <h2 class="tm-blue-text tm-welcome-title tm-margin-b-45">Welcome to Living Room Design</h2>
+                                </header>
+                                <p>A living room is a room in a home. The living room in a home is shared by the people living in the home. In the living room they talk with each other and their guests and do other things such as reading or watching television. The furniture can include a couch, chairs, tables, lamps, a television, curtains and pictures. People may have started using the compound word, living room, in the early twentieth century.</p>
+                            </section>
+                            <!-- About section -->
+                            <section id="about" class="tm-section">
+                                <div class="row">                                                                
+                                    <div class="col-lg-8 col-md-7 col-sm-12 col-xs-12 push-lg-4 push-md-5">
+                                        <header>
+                                            <h2 class="tm-blue-text tm-section-title tm-margin-b-45">About our project</h2>
+                                        </header>
+                                        <p>We build a .......</p>
+                                        <p>by done this...........
+                                        .....................................</p>
+                                        <a href="#" class="tm-button tm-button-wide">Read More</a>  
+                                    </div>
+
+                                    <div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 pull-lg-8 pull-md-7 tm-about-img-container">
+                                        
+                                    </div>  
+                                </div>                            
+                            </section>  
+
+                            <!-- Gallery One section -->     
+                            <section id="galleryone" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Gallery One</h2></header>
+                                <div class="tm-gallery-container tm-gallery-1">
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/live1.jpg"><img src="img/live1.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/live 2.jpg"><img src="img/live 2.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/live 3.jpg"><img src="img/live 3.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/live 4.jpg"><img src="img/live 4.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-1">
+                                        <a href="img/live 5.jpg"><img src="img/live 5.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>                                    
+                                </div>
+                            </section>
+
+                            <!-- Second Gallery section -->     
+                            <section id="secondgallery" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Second Gallery</h2></header>
+                                <div class="tm-gallery-container tm-gallery-2">
+                                    <div class="tm-img-container tm-img-container-2">
+                                        <a href="img/live 6.jpg"><img src="img/live 6.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-2">
+                                        <a href="img/live 7.jpg"><img src="img/live 7.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-2">
+                                        <a href="img/live 8.jpg"><img src="img/live 8.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>                                    
+                                </div>
+                            </section>
+
+                            <!-- Third Gallery section -->     
+                            <section id="thirdgallery" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Third Gallery</h2></header>
+                                <div class="tm-gallery-container tm-gallery-3">
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/live 9.jpg"><img src="img/live 9.jpg" alt="Image" class="img-fluid tm-img-tn"></a>    
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/live10.jpg"><img src="img/live10.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/live11.jpg"><img src="img/live11.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+                                    </div>
+                                    <div class="tm-img-container tm-img-container-3">
+                                        <a href="img/live12.jpg"><img src="img/live12.jpg" alt="Image" class="img-fluid tm-img-tn"></a>
+                                    </div>                                    
+                                </div>
+                            </section>
+
+                            <!-- Contact Us section -->
+                            <section id="contact" class="tm-section">
+                                <header><h2 class="tm-blue-text tm-section-title tm-margin-b-30">Contact Us</h2></header>
+
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <form action="#contact" method="post" class="contact-form">
+                                            <div class="form-group">
+                                                <input type="text" id="contact_name" name="contact_name" class="form-control" placeholder="Name"  required/>
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="email" id="contact_email" name="contact_email" class="form-control" placeholder="Email"  required/>
+                                            </div>
+                                            <div class="form-group">
+                                                <textarea id="contact_message" name="contact_message" class="form-control" rows="9" placeholder="Message" required></textarea>
+                                            </div>                                            
+                                            <button type="submit" class="float-right tm-button">Send</button>
+                                        </form>    
+                                    </div>
+                                    
+                                    <div class="col-lg-6 tm-contact-right">
+                                        <p>
+                                        Infinity Software Company Limited a company that.... 
+                                        </p>
+                                        <address>
+                                            Infinity Software Company Limited<br>
+                                            Road No- 7/A, House-185/Kha, Dhanmondi, Dhaka-1209. Cell: +8801760150897<br>
+                                            
+                                        </address>
+                                    </div>
+                                </div>
+                                
+                            </section>
+                            <footer>
+                                <p class="tm-copyright-p">Copyright &copy; <span class="tm-current-year">2018</span> Infinity Software Company Limited
+                                
+                               
+                            </footer>
+                        </div>  
+                        
+                    </div> <!-- Right column: content -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+                
+        <!-- load JS files -->
+        <script src="js/jquery-1.11.3.min.js"></script>             <!-- jQuery (https://jquery.com/download/) -->
+        <script src="js/jquery.magnific-popup.min.js"></script>     <!-- Magnific pop-up (http://dimsemenov.com/plugins/magnific-popup/) -->
+        <script src="js/jquery.singlePageNav.min.js"></script>      <!-- Single Page Nav (https://github.com/ChrisWojcik/single-page-nav) -->
+        <script>     
+       
+            $(document).ready(function(){
+
+                // Single page nav
+                $('.tm-main-nav').singlePageNav({
+                    'currentClass' : "active",
+                    offset : 20
+                });
+
+                // Magnific pop up
+                $('.tm-gallery-1').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-gallery-2').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-gallery-3').magnificPopup({
+                  delegate: 'a', // child items selector, by clicking on it popup will open
+                  type: 'image',
+                  gallery: {enabled:true}
+                  // other options
+                }); 
+
+                $('.tm-current-year').text(new Date().getFullYear());                
+            });
+        </script> 
+        <div id="wrapper">
+
+      <a href="https://www.facebook.com/Infinity-Software-Company-Limited-1994132243972041/?ref=bookmarks" target="blank"><div class="box" id="facebook">&#62220;</div></a>
+      <a href="https://twitter.com/InfinitySoftwa3" target="blank"><div class="box" id="twitter">&#62217;</div></a>
+       <a href="https://www.instagram.com/infinitysoftwarec/" target="blank"><div class="box" id="instagram">&#62253;</div></a>             
+</body>
+</html>
