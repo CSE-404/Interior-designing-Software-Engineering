7ed8541ffc683919d54d683a402b58b4c828bb9b
tareqrahman111 <40437000+tareqrahman111@users.noreply.github.com>
2018-09-02 03:43:14 +0600

    Add files via upload

---
+++ interior_design/app/Http/Controllers/Index3PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Index3PageController extends Controller{
+
+    public function showIndex3page()
+    {
+        return view('index3');
+    }
+}
