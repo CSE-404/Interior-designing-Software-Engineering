98e7fccae7ffbc04415e5ec33562c6be8cdb394e
tareqrahman111 <40437000+tareqrahman111@users.noreply.github.com>
2018-09-02 03:28:51 +0600

    Update index.blade.php

--- interior_design/resources/views/index.blade.php
+++ interior_design/resources/views/index.blade.php
@@ -36,7 +36,7 @@ http://www.templatemo.com/tm-501-neaty
                         <ul>
                             <li><a href="{{ url('/index') }}" class="active1">Kitchen</a></li>
                             <li><a href="{{ url('/index2') }}">Home</a></li>
-                            <li><a href="#">Office</a></li>
+                            <li><a href="{{ url('/index3') }}">Office</a></li>
                             <li><a href="#">Living room</a></li>
                             <li><a href="#">Bedroom</a></li>
                         </ul>
