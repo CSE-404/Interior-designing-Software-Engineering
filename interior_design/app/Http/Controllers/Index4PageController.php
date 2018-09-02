ddec2c7a057ff42ba09484098c2996860cb18555
Md. Asifuzzaman Jishan <32738421+jishan900@users.noreply.github.com>
2018-09-02 16:27:44 +0600

    Add files via upload

---
+++ interior_design/app/Http/Controllers/Index4PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Index4PageController extends Controller{
+
+    public function showIndex4page()
+    {
+        return view('index4');
+    }
+}
