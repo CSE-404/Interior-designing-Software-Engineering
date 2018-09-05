ea00e7659b5b58b18abfb8b496903417f019633a
Md. Asifuzzaman Jishan <32738421+jishan900@users.noreply.github.com>
2018-09-05 19:37:06 +0600

    Add files via upload

---
+++ interior_design/app/Http/Controllers/Index5PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Index5PageController extends Controller{
+
+    public function showIndex5page()
+    {
+        return view('index5');
+    }
+}
