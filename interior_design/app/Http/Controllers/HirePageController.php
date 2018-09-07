3583e3862c327291daf7e4c7bc06a7ae47bd9c27
Md. Asifuzzaman Jishan <32738421+jishan900@users.noreply.github.com>
2018-09-08 02:02:36 +0600

    Update HirePageController.php

--- interior_design/app/Http/Controllers/HirePageController.php
+++ interior_design/app/Http/Controllers/HirePageController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Hire;
+
 class HirePageController extends Controller{
 
     public function showHirepage()
     {
-        return view('hire');
+    	$hires = Hire::all();
+        return view('hire', compact('hires'));
     }
+
+    // public function getData()
+    // {
+    // 	$data['data'] = DB::table('designer')->get();
+    // }
 }
