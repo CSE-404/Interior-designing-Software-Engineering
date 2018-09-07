016ef8f215b1309be768edccd8dd2706613cd143
Md. Asifuzzaman Jishan <32738421+jishan900@users.noreply.github.com>
2018-09-08 01:54:18 +0600

    Update hire.blade.php

--- interior_design/resources/views/hire.blade.php
+++ interior_design/resources/views/hire.blade.php
@@ -40,19 +40,19 @@
                 <li>
                     <span>
           <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
-        </span><a href="{{ url('hire')}}">Hire a designer</a>
+        </span><a href="{{ url('/hire?id=1-8')}}">My Designer</a>
                 </li>
                 <li>
                     <span>
           <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
-        </span><a href="{{ url('steam')}}">Our Suppliers Team</a>
-                </li>
-                <li>
-                    <span>
-          <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
+     
         </span><a href="{{ url('producto')}}">Product Order</a>
                 </li>
                 <li>
+                    <span>
+          <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
+        </span><a href="{{ url('/myproduct?id=1-8')}}">My Product</a>
+                </li><li>
                     <span>
           <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" >
         </span><a href="{{ url('contactu')}}">Contact us</a>
@@ -65,228 +65,15 @@
             <h1>Hiring Section</h1>
             <p>Interior design is the art and science of enhancing the interior of a building to achieve a healthier and more aesthetically pleasing environment for the people using the space. An interior designer is someone who plans, researches, coordinates, and manages such projects. Interior design is a multifaceted profession that includes conceptual development, space planning, site inspections, programming, research, communicating with the stakeholders of a project, construction management, and execution of the design.</p>
             <div id="box">
-                <div class="box-top">TERMS & CONDITIONS OF 
-INTERIOR DESIGN SERVICE</div>
+                <div class="box-top">Hire Designer</div>
                 <div class="box-panel">
-These conditions shall apply to the Contract for the supply of goods and/or services by the supplier 
-–
-“Genesis Red Earth
-Ltd T/a Red Earth
-”, to the 
-person to whom they are supplied
-________________
-________
-(Name) of _________________________________________________ (Address)
-, 
-to the exclusion of all other terms and conditions including any terms and conditions which the Cli
-ent may purport to apply under any purchase order 
-confirmation or order of similar document.<br><br>
-1.
-In performing professional services, 
-Genesis Red Earth
-Ltd
-. shall exercise reasonable 
-care and competence and shall carry out its duties in a 
-professional manner.
-It is the customer’s responsibility to ensure that there is sufficient and safe access for items selected
-and for Red Earth 
-staff
-to the designated area and that there is sufficient access for our delivery lorry to reach the delivery address.<br>
-2.
-We charge an 
-initial call out fee of €150 for the Dublin and Greater Dublin area.  
-This €150 can be redeemed when a spend of €2,000 on Mindy 
-Brownes Products is reached. 
-Quotes for other area around Ireland will need to be requested from our Interior Designer.
-Additional call out 
-fees may be charged depending on location, this will be agreed with the Interior 
-Designer at the time of contract and order.<br>
-3.
-Payment Terms:<br><br>
-a.
-A minimum deposit of 50% is required on acceptance of design scheme, this is subject to change
-depending on scale of custom 
-products on order but will be advised to you in advance of design scheme acceptance.<br>
-b.
-An additional instalment of 40% is required on completion of customised and bespoke products
-and prior to delivery.<br>
-c.
-Balance instalment of 10%
-is required on completion of 
-scheme in designated area.<br><br>
-4.
-The Client will supply all necessary information and give all necessary ap
-provals/decisions to the 
-Interior Designer for the proper and timely 
-execution of the Project.
-If ins
-tructions are not 
-forth
--
-coming the Interior Designer cannot be held respo
-n
-sible for ensuing delays.<br>
-5.
-The Client will appoint and reimburse for the services of any other consultant
-or craftsman
-as requested by the 
-client.
-The Interior Designer 
-will not be responsible in any 
-way for th
-e competence and performance 
-of their services.
-In certain circumstances where 
-Red Earth
-employs a 
-specialist or craf
-tsman direct,
-the Interior Designer will be responsible for the proper execution of their work.<br>
-6.
-No right or licence is granted u
-nder the Contract to the Client under any patent
-, trademark, copyright, 
-registered design or other intellectual 
-property right except the right to use 
-the goods supplied.
-In 
-particular the copyright of the Interior Designer in designs supplied remains wit
-h 
-the Interior Designer.<br>
-7.
-The Interior Designer shall not be liable for any loss or damage whatsoever 
-arising from termination 
-of appointment.
-Any such notice shall be 
-given by either the Client or the Interior Designer in writing.<br>
-8.
-Times and dates quoted 
-for delivery of goods or materials or completion of an
-y work are to be treated 
-as an approximate estimate based on 
-current trading conditions and the Interior Desi
-gner shall not 
-be held responsible for delays outside its control.
-The lead time quoted is 
-ap
-proximate only and given in good faith. Although Genesis Red Earth Limited will endeavour to keep to this time frame, if good
-s are delayed 
-for any reason we cannot accept liability for consequential losses as a result.<br>
-9.
-The Interior Designer will not be hel
-d responsible for client’s goods left at the 
-premises of the Interior 
-Designer or his suppliers for whatever 
-reason.
-Clients are responsible for th
-e insurance of their own 
-goods with their own insurance company.<br>
-10.
-Special custom made items cannot be cancell
-ed once manufacturing
-has commenced. Payment for all 
-such items
-is required in full
-.
-Standard 
-items might be subject to a canc
-ellation charge from the 
-manufacturer or supplier if cancelled.<br>
-11.
-“Risk & Title”
--
-the risk to the goods shall pass to the buyer upo
-n delivery of the goods to your entrance door at the agreed address. The title of 
-the goods shall not pass from Genesis Red Earth Limited until the price of the goods and services ordered by the customer fro
-m Genesis Red 
-Earth Limited is paid in full.
-Good
-s may not be rejected for any reason other than that they are defective.<br>
-12.
-Variances in wood, marble and leather
-etc
-, are to be expected as no two pieces of furniture, cut from unique materials, can be identical to those 
-that are on display.<br>
-13.
-If you chose to 
-relocate your furniture to any private storage facility directly from the Genesis Red Earth Limited warehouse, Genesis Red Ea
-rth 
-Limited will deliver your furniture to this location, However, on the arrival of your furniture to the elected facility, no f
-ur
-ther liability or 
-relocation obligation in respect of your furniture rests with Genesis Red Earth Limited.<br>
-14.
-A delivery charge may have been included in your order, any such charges will have been 
-discussed and agreed with you
-. Full details of our 
-delivery c
-harges are available on request.<br>
-15.
-We recommend the use of felt pads, to be put on all furniture legs, and to take extra care when moving furniture around the r
-oom to avoid 
-unnecessary damage to wooden or tiled floors.<br>
-16.
-Each delivery of goods must be inspecte
-d by the customer or nominated representative to confirm receipt of goods. Any complaints in relation 
-to goods having suffered accidental damage prior to delivery must be notified to Genesis Red Earth Limited within 4 days.<br>
-17.
-In order to complete and deliver
-your 
-interior design
-project
-, we (Genesis Red Earth Limited) may need to share your contact details with our 
-suppliers appointed delivery team and technicians. You hereby agree and consent to the provision of such details (without fur
-ther contact with 
-y
-ou
-) to the delivery team and
-technicians for this purpose.<br>
-18.
-In the event that you notify us of an issue and/ or defect with your furniture order post
--
-delivery, we (Genesis Red Earth 
-Limited
-) may need to 
-share your contact and delivery details with our/our 
-suppliers appointed technicians in order for the technicians to assess, inspect, repair, 
-remove or replace such product. You hereby agree and consent to the provision of such details (without further contact with y
-ou) to the 
-technicians for this purpose.<br>
-19.
-I
-n the event that a product defect or health and safety concern is notified to us (Genesis Red Earth 
-Limited
-) in relation to the product which you 
-have ordered/purchased, you will be notified by us all as soon as is reasonable practicable. In such circumsta
-nces, you hereby a
-gree and consent 
-to the provision of your contact details (without further contact with you) to the product supplier and/or other necessary/ r
-elevant third parties, 
-strictly for the purposes of assessing, inspecting, repairing, removing o
-r replacing such product on order to prevent injury or other damages to 
-your health and/or prevent serious loss or damage to your property.<br>
-20.
-Your contact details will not be provided by us (Genesis Red Earth 
-Limited
-) to the product supplier and/or other nec
-essary relevant third parties 
-for any other purpose (other than those expressly set out in this agreement) without your prior agreement.<br>
-21.
-Genesis Red Earth 
-Limited
-will comply with its data controller’s obligations in respect of contact details provided and
-the lawful processing 
-thereof, having regard to the obligations imposed by the Data Protection Act 1998 and 2003.<br>
-22.
-These terms and conditions do not affect your statutory rights under the Sale of Goods and Supply of Services Act 1980.<br>
-23.
-These terms and condi
-tions shall be governed by and construed in accordance with the laws of the Republic of Ireland. 
-Please sign 
-to indicate your acceptance of
-the
-above
-terms and conditions.</div>
+                    @foreach($hires as $data)
+                        @if($data['id']==$_GET['id'])
+                        {{$data['name']}}
+                        @endif
+                    @endforeach
+
+                </div>
             </div>
         </div>
 
